cc5313be795517810704e3ac44e54f017f1621d1
Noah Medoff and Randy Weyer <noah@>
2018-03-20 15:17:16 -0700

    added functionality to all the cards

---
+++ js/scripts.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+  // Test Card Toggle
+  $('#test-card').click(function() {
+    $('#testy').toggle();
+  });
+  // JavaScript Card Toggle
+  $('#javascript').click(function() {
+    $('#javascript-definition').toggle();
+  });
+  //Operators Card Toggle
+  $('#operators').click(function() {
+    $('#operators-definition').toggle();
+  });
+  // Variables Card Toggle
+  $('#variables').click(function() {
+    $('#variables-definition').toggle();
+  });
+  //Variable naming convention Card Toggle
+  $('#variable-naming-conventions').click(function() {
+    $('#variable-naming-conventions-definition').toggle();
+  });
+  //Function Card Toggle
+  $('#functions').click(function() {
+    $('#functions-definition').toggle();
+  });
+  // Methods Card Toggle
+  $('#methods').click(function() {
+    $('#methods-definition').toggle();
+  });
+  // Arguments Card Toggle
+  $('#arguments').click(function() {
+    $('#arguments-definition').toggle();
+  });
+  // Parameters Card Toggle
+  $('#parameters').click(function() {
+    $('#parameters-definition').toggle();
+  });
+  //Return Card Toggle
+  $('#return').click(function() {
+    $('#return-definition').toggle();
+  });
+  //Chaining Methods Card Toggle
+  $('#chaining-methods').click(function() {
+    $('#chaining-methods-definition').toggle();
+  });
+  // Strings Card Toggle
+  $('#strings').click(function() {
+    $('#strings-definition').toggle();
+  });
+  // Booleans Card Toggle
+  $('#booleans').click(function() {
+    $('#booleans-definition').toggle();
+  });
+  // Unidentified Card Toggle
+  $('#unidentified').click(function() {
+    $('#unidentified-definition').toggle();
+  });
+  // NaN Card Toggle
+  $('#nan').click(function() {
+    $('#nan-definition').toggle();
+  });
+  // Escape Card Toggle
+  $('#escape').click(function() {
+    $('#escape-definition').toggle();
+  });
+  // Alert Card Toggle
+  $('#alert').click(function() {
+    $('#alert-definition').toggle();
+  });
+  // Comments Card Toggle
+  $('#comments').click(function() {
+    $('#comments-definition').toggle();
+  });
+  // jQuery Card Toggle
+  $('#jquery').click(function() {
+    $('#jquery-definition').toggle();
+  });
+  // Attributes Card Toggle
+  $('#attributes').click(function() {
+    $('#attributes-definition').toggle();
+  });
+});
